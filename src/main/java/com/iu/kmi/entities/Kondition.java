24f6f4010415d072780d36502246b2ff6d29a130
Julian <julian.treichel@mitel.com>
2024-06-06 18:10:28 +0200

    adjusted comments

--- src/main/java/com/iu/kmi/entities/Kondition.java
+++ src/main/java/com/iu/kmi/entities/Kondition.java
@@ -1,7 +1,7 @@
 package com.iu.kmi.entities;
 
 /**
- * Repräsentiert eine Menge von Bedingungen einschließlich Zahlungsmethode, Lieferbedingungen und Rabatt.
+ * Repräsentiert eine Menge von Konditionen einschließlich Zahlungsmethode, Lieferbedingungen und Rabatt.
  */
 public class Kondition {
     private int id;
@@ -11,12 +11,12 @@ public class Kondition {
     private double rabatt;
 
     /**
-     * Konstruktor für eine neue BedingungEntitaet mit dem angegebenen Namen, Zahlungsmethode, Lieferbedingungen und Rabatt.
+     * Konstruktor für eine neue Kondition mit dem angegebenen Namen, Zahlungsmethode, Lieferbedingungen und Rabatt.
      *
-     * @param name der Name der Bedingung
-     * @param zahlungsmethode die Zahlungsmethode, die mit der Bedingung verbunden ist
-     * @param lieferbedingungen die Lieferbedingungen, die mit der Bedingung verbunden sind
-     * @param rabatt der Rabatt, der mit der Bedingung verbunden ist
+     * @param name der Name der Kondition
+     * @param zahlungsmethode die Zahlungsmethode, die mit der Kondition verbunden ist
+     * @param lieferbedingungen die Lieferbedingungen, die mit der Kondition verbunden sind
+     * @param rabatt der Rabatt, der mit der Kondition verbunden ist
      */
     public Kondition(String name, String zahlungsmethode, String lieferbedingungen, double rabatt) {
         this.name = name;
@@ -26,90 +26,90 @@ public class Kondition {
     }
 
     /**
-     * Gibt die ID der Bedingung zurück.
+     * Gibt die ID der Kondition zurück.
      *
-     * @return die ID der Bedingung
+     * @return die ID der Kondition
      */
     public int getId() {
         return id;
     }
 
     /**
-     * Setzt die ID der Bedingung.
+     * Setzt die ID der Kondition.
      *
-     * @param id die neue ID der Bedingung
+     * @param id die neue ID der Kondition
      */
     public void setId(int id) {
         this.id = id;
     }
 
     /**
-     * Gibt den Namen der Bedingung zurück.
+     * Gibt den Namen der Kondition zurück.
      *
-     * @return der Name der Bedingung
+     * @return der Name der Kondition
      */
     public String getName() {
         return name;
     }
 
     /**
-     * Setzt den Namen der Bedingung.
+     * Setzt den Namen der Kondition.
      *
-     * @param name der neue Name der Bedingung
+     * @param name der neue Name der Kondition
      */
     public void setName(String name) {
         this.name = name;
     }
 
     /**
-     * Gibt die Zahlungsmethode zurück, die mit der Bedingung verbunden ist.
+     * Gibt die Zahlungsmethode zurück, die mit der Kondition verbunden ist.
      *
-     * @return die Zahlungsmethode, die mit der Bedingung verbunden ist
+     * @return die Zahlungsmethode, die mit der Kondition verbunden ist
      */
     public String getZahlungsmethode() {
         return zahlungsmethode;
     }
 
     /**
-     * Setzt die Zahlungsmethode, die mit der Bedingung verbunden ist.
+     * Setzt die Zahlungsmethode, die mit der Kondition verbunden ist.
      *
-     * @param zahlungsmethode die neue Zahlungsmethode, die mit der Bedingung verbunden ist
+     * @param zahlungsmethode die neue Zahlungsmethode, die mit der Kondition verbunden ist
      */
     public void setZahlungsmethode(String zahlungsmethode) {
         this.zahlungsmethode = zahlungsmethode;
     }
 
     /**
-     * Gibt die Lieferbedingungen zurück, die mit der Bedingung verbunden sind.
+     * Gibt die Lieferbedingungen zurück, die mit der Kondition verbunden sind.
      *
-     * @return die Lieferbedingungen, die mit der Bedingung verbunden sind
+     * @return die Lieferbedingungen, die mit der Kondition verbunden sind
      */
     public String getLieferbedingungen() {
         return lieferbedingungen;
     }
 
     /**
-     * Setzt die Lieferbedingungen, die mit der Bedingung verbunden sind.
+     * Setzt die Lieferbedingungen, die mit der Kondition verbunden sind.
      *
-     * @param lieferbedingungen die neuen Lieferbedingungen, die mit der Bedingung verbunden sind
+     * @param lieferbedingungen die neuen Lieferbedingungen, die mit der Kondition verbunden sind
      */
     public void setLieferbedingungen(String lieferbedingungen) {
         this.lieferbedingungen = lieferbedingungen;
     }
 
     /**
-     * Gibt den Rabatt zurück, der mit der Bedingung verbunden ist.
+     * Gibt den Rabatt zurück, der mit der Kondition verbunden ist.
      *
-     * @return der Rabatt, der mit der Bedingung verbunden ist
+     * @return der Rabatt, der mit der Kondition verbunden ist
      */
     public double getRabatt() {
         return rabatt;
     }
 
     /**
-     * Setzt den Rabatt, der mit der Bedingung verbunden ist.
+     * Setzt den Rabatt, der mit der Kondition verbunden ist.
      *
-     * @param rabatt der neue Rabatt, der mit der Bedingung verbunden ist
+     * @param rabatt der neue Rabatt, der mit der Kondition verbunden ist
      */
     public void setRabatt(double rabatt) {
         this.rabatt = rabatt;
@@ -122,7 +122,7 @@ public class Kondition {
      */
     @Override
     public String toString() {
-        return "BedingungEntitaet{" +
+        return "KonditionEntitaet{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", zahlungsmethode='" + zahlungsmethode + '\'' +
