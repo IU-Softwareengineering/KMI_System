f1063fd4f2c58246a765c257a103fa093cd524ec
Julian <julian.treichel@mitel.com>
2024-06-06 14:58:59 +0200

    Added initial Konditionen class and KonditionenTest class

---
+++ src/main/java/com/iu/kmi/test/KonditionenTest.java
@@ -0,0 +1,42 @@
+package JUint_tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import com.iu.kmi.data.entity.Konditionen;
+
+public class KonditionenTest {
+
+    @Test
+    public void testConstructorAndGetters() {
+        Konditionen konditionen = new Konditionen("Standard", "Kreditkarte", "Lieferung in 5 Tagen", 10.0);
+        assertEquals("Standard", konditionen.getName());
+        assertEquals("Kreditkarte", konditionen.getZahlungsmethode());
+        assertEquals("Lieferung in 5 Tagen", konditionen.getLieferbedingungen());
+        assertEquals(10.0, konditionen.getRabatt());
+    }
+
+    @Test
+    public void testSetters() {
+        Konditionen konditionen = new Konditionen("Standard", "Kreditkarte", "Lieferung in 5 Tagen", 10.0);
+        konditionen.setId(1);
+        konditionen.setName("Premium");
+        konditionen.setZahlungsmethode("PayPal");
+        konditionen.setLieferbedingungen("Lieferung in 2 Tagen");
+        konditionen.setRabatt(15.0);
+
+        assertEquals(1, konditionen.getId());
+        assertEquals("Premium", konditionen.getName());
+        assertEquals("PayPal", konditionen.getZahlungsmethode());
+        assertEquals("Lieferung in 2 Tagen", konditionen.getLieferbedingungen());
+        assertEquals(15.0, konditionen.getRabatt());
+    }
+
+    @Test
+    public void testToString() {
+        Konditionen konditionen = new Konditionen("Standard", "Kreditkarte", "Lieferung in 5 Tagen", 10.0);
+        konditionen.setId(1);
+        String expected = "BedingungEntitaet{id=1, name='Standard', zahlungsmethode='Kreditkarte', lieferbedingungen='Lieferung in 5 Tagen', rabatt=10.0}";
+        assertEquals(expected, konditionen.toString());
+    }
+}
