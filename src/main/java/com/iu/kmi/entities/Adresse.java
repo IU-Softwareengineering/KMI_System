47fb5257cc91aa89fbc6b065b6e265de5161fead
Othman Al Daood <othman3mm@gmail.com>
2024-06-14 00:37:24 +0200

    fix
    fixe

--- src/main/java/com/iu/kmi/entities/Adresse.java
+++ src/main/java/com/iu/kmi/entities/Adresse.java
@@ -28,10 +28,7 @@ public class Adresse {
         this.land = land;
     }
 
-    Adresse(String musterstraße_1, String string, String musterstadt) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
+ 
     // Getter and Setter methods
     /**
      * Gets the street name and number.
