52fca596cc20a6f7285ca0c689560b8797145ddb
joschkoa <anthony.joschko@mitel.com>
2024-06-14 12:02:29 +0200

    added Kondition saving

---
+++ src/main/java/com/iu/kmi/repositories/KonditionRepository.java
@@ -0,0 +1,8 @@
+package com.iu.kmi.repositories;
+
+import com.iu.kmi.database.repository.Repository;
+import com.iu.kmi.entities.Kondition;
+
+public interface KonditionRepository extends Repository<Kondition, Integer>{
+
+}
