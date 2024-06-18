77f9954a4a994ae587bb035e3d4cd5d16a052170
Othman Al Daood <othman3mm@gmail.com>
2024-06-18 23:17:20 +0200

    Kunde Repository erstellen

---
+++ src/main/java/com/iu/kmi/database/repository/KundeRepository.java
@@ -0,0 +1,11 @@
+package com.iu.kmi.database.repository;
+
+import com.iu.kmi.entities.Kunde;
+
+/**
+ * Repository interface for the Kunde entity. Provides CRUD operations.
+ *
+ * @author Othman
+ */
+public interface KundeRepository extends Repository<Kunde, String> {
+}
