9d263bb9ef02b70ae4c29fd5ddf612e3f33a19f8
Othman Al Daood <othman3mm@gmail.com>
2024-06-18 23:14:06 +0200

    Kunde Repository
    Kunde Repository

---
+++ src/main/java/com/iu/kmi/database/repository/KundeRepository.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.iu.kmi.database.repository;
+
+import com.iu.kmi.entities.Kunde;
+
+/**
+ * Repository interface for the Kunde entity. Provides CRUD operations.
+ *
+ * @author Othman
+ */
+public interface KundeRepository extends Repository<Kunde, String> {
+}
