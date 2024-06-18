97dab0c73c4ba3533e4c3c434a3daa49ba94f532
Othman Al Daood <othman3mm@gmail.com>
2024-06-18 23:06:35 +0200

    Repository Adresse erstellen

---
+++ src/main/java/com/iu/kmi/database/repository/AdresseRepository.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.iu.kmi.database.repository;
+
+import com.iu.kmi.entities.Adresse;
+
+/**
+ * Repository interface for the Adresse entity. Provides CRUD operations.
+ *
+ * @author Othman
+ */
+public interface AdresseRepository extends Repository<Adresse, String> {
+}
