b0128da5c562d8a9faee57be36e0d4b7dad263fb
AnastaciaBe <136445376+AnastaciaBe@users.noreply.github.com>
2024-06-19 21:06:15 +0200

    Create LagerbestandRepository.java

---
+++ src/main/java/com/iu/kmi/repositories/LagerbestandRepository.java
@@ -0,0 +1,8 @@
+package com.iu.kmi.repositories;
+
+import com.iu.kmi.database.repository.Repository;
+import com.iu.kmi.entities.Lagerbestand;
+
+public interface LagerbestandRepository extends Repository<Lagerbestand, String>{
+
+}
