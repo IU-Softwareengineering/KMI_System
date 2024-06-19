226042505bc74001d667e5e17dd0438b58c15d79
AnastaciaBe <136445376+AnastaciaBe@users.noreply.github.com>
2024-06-19 21:06:20 +0200

    Update Lager.java

--- src/main/java/com/iu/kmi/entities/Lager.java
+++ src/main/java/com/iu/kmi/entities/Lager.java
@@ -1,56 +1,116 @@
 package com.iu.kmi.entities;
 
+import com.iu.kmi.database.annotations.Column;
+import com.iu.kmi.database.annotations.Entity;
+import com.iu.kmi.database.annotations.Id;
+
+/**
+ * Repräsentiert eine Lager-Entität.
+ * Diese Klasse wird auf die "lager" Tabelle in der Datenbank abgebildet.
+ */
+@Entity(tableName = "lager")
 public class Lager {
 
     // Attribute
-    private String lagerNummer;
+    /**
+     * Die eindeutige Kennung für das Lager.
+     */
+    @Id(name = "lager_nr")
+    private String lager_nr;
+
+    /**
+     * Der Name des Lagers.
+     */
+    @Column(name = "name")
     private String name;
-    private int adresse;
+
+    /**
+     * Die Adresskennung für das Lager.
+     */
+    @Column(name = "adresse_nr_nr")
+    private int adresse_nr;
 
     // Konstruktor
-    public Lager(String lagerNummer, String name, int adresse) {
-        this.lagerNummer = lagerNummer;
+    /**
+     * Erstellt eine neue Lager-Instanz mit den angegebenen Details.
+     *
+     * @param lager_nr die eindeutige Kennung für das Lager
+     * @param name der Name des Lagers
+     * @param adresse_nr die Adresskennung für das Lager
+     */
+    public Lager(String lager_nr, String name, int adresse_nr) {
+        this.lager_nr = lager_nr;
         this.name = name;
-        this.adresse = adresse;
+        this.adresse_nr = adresse_nr;
     }
-
 
     // Getter-Methoden
+    /**
+     * Gibt die eindeutige Kennung des Lagers zurück.
+     *
+     * @return die eindeutige Kennung des Lagers
+     */
     public String getLagerNummer() {
-        return lagerNummer;
+        return lager_nr;
     }
 
+    /**
+     * Gibt den Namen des Lagers zurück.
+     *
+     * @return der Name des Lagers
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gibt die Adresskennung des Lagers zurück.
+     *
+     * @return die Adresskennung des Lagers
+     */
     public int getAdresse() {
-        return adresse;
+        return adresse_nr;
     }
-
 
     // Setter-Methoden
-    public void setLagerNummer(String lagerNummer) {
-        this.lagerNummer = lagerNummer;
+    /**
+     * Setzt die eindeutige Kennung für das Lager.
+     *
+     * @param lager_nr die eindeutige Kennung für das Lager
+     */
+    public void setLagerNummer(String lager_nr) {
+        this.lager_nr = lager_nr;
     }
-    
+
+    /**
+     * Setzt den Namen des Lagers.
+     *
+     * @param name der Name des Lagers
+     */
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setAdresse(int adresse) {
-        this.adresse = adresse;
+    /**
+     * Setzt die Adresskennung für das Lager.
+     *
+     * @param adresse_nr die Adresskennung für das Lager
+     */
+    public void setAdresse(int adresse_nr) {
+        this.adresse_nr = adresse_nr;
     }
 
-
-
+    /**
+     * Gibt eine Zeichenkettendarstellung des Lagers zurück.
+     *
+     * @return eine Zeichenkettendarstellung des Lagers
+     */
     @Override
     public String toString() {
         return "Lager{" +
-                "lagerNummer=" + lagerNummer +
+                "lager_nr=" + lager_nr +
                 ", name='" + name + '\'' +
-                ", adresse=" + adresse +
+                ", adresse_nr=" + adresse_nr +
                 '}';
     }
-
 }
