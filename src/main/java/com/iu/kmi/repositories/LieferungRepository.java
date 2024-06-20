e9ef1a4a86ad9fa3c3a3590123e5c5d4696989cb
Julian <julian.treichel@mitel.com>
2024-06-20 12:18:15 +0200

    added author comments

--- src/main/java/com/iu/kmi/repositories/LieferungRepository.java
+++ src/main/java/com/iu/kmi/repositories/LieferungRepository.java
@@ -2,5 +2,9 @@ package com.iu.kmi.repositories;
 
 import com.iu.kmi.database.repository.Repository;
 
+/**
+ * @author Julian Treichel
+ * @since 20.06.2024
+ */
 public interface LieferungRepository extends Repository<LieferungRepository, Integer>{
 }
--- src/main/java/com/iu/kmi/repositories/RechnungRepository.java
+++ src/main/java/com/iu/kmi/repositories/RechnungRepository.java
@@ -3,5 +3,9 @@ package com.iu.kmi.repositories;
 import com.iu.kmi.database.repository.Repository;
 import com.iu.kmi.entities.Rechnung;
 
+/**
+ * @author Julian Treichel
+ * @since 20.06.2024
+ */
 public interface RechnungRepository extends Repository<Rechnung,Integer>{
 }
