e8f4d25720e3cc67d8aa4cae9df7b9e3e4ceaf90
Julian <julian.treichel@mitel.com>
2024-06-20 11:39:54 +0200

    added AuftragRespository

---
+++ src/main/java/com/iu/kmi/repositories/AuftragRespository.java
@@ -0,0 +1,7 @@
+package com.iu.kmi.repositories;
+
+import com.iu.kmi.database.repository.Repository;
+import com.iu.kmi.entities.Auftrag;
+
+public interface AuftragRespository extends Repository<Auftrag, Integer>{
+}
