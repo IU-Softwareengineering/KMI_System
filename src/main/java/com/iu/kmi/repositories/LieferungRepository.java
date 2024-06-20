81a659c55385c686854529bdad8558c9e130ce65
AnastaciaBe <136445376+AnastaciaBe@users.noreply.github.com>
2024-06-20 12:17:33 +0200

    repos

--- src/main/java/com/iu/kmi/repositories/LieferungRepository.java
+++ src/main/java/com/iu/kmi/repositories/LieferungRepository.java
@@ -2,5 +2,5 @@ package com.iu.kmi.repositories;
 
 import com.iu.kmi.database.repository.Repository;
 
-public interface LieferungRepository extends Repository<LieferungRepository, Integer>{
+public interface LieferungRepository extends Repository<LieferungRepository, String>{
 }
