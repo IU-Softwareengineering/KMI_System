4ac360825d09afd121720e5ce5f9944d59ba01f9
Julian <julian.treichel@mitel.com>
2024-06-21 10:49:08 +0200

    adjusted LieferungRepository

---
+++ src/main/java/com/iu/kmi/repositories/LieferungRepository.java
@@ -0,0 +1,10 @@
+package com.iu.kmi.repositories;
+
+import com.iu.kmi.database.repository.Repository;
+
+/**
+ * @author Julian Treichel
+ * @since 20.06.2024
+ */
+public interface LieferungRepository extends Repository<LieferungRepository, String>{
+}
