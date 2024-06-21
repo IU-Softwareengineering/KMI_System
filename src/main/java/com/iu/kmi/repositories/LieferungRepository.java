f4e165523a0b3ef0765c4adf4eb61dd663e48970
h0rzn <hrzn@protonmail.com>
2024-06-21 11:17:51 +0200

    fixed repositories

--- src/main/java/com/iu/kmi/repositories/LieferungRepository.java
+++ src/main/java/com/iu/kmi/repositories/LieferungRepository.java
@@ -1,10 +1,11 @@
 package com.iu.kmi.repositories;
 
 import com.iu.kmi.database.repository.Repository;
+import com.iu.kmi.entities.Lieferung;
 
 /**
  * @author Julian Treichel
  * @since 20.06.2024
  */
-public interface LieferungRepository extends Repository<LieferungRepository, String>{
+public interface LieferungRepository extends Repository<Lieferung, String>{
 }
