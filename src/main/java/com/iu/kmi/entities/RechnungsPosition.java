0f7e350050f16bf97467b89df59080bf17efadc5
Florian <123985169+dl25dev@users.noreply.github.com>
2024-06-27 18:03:41 +0200

    Rechnungsinterface

---
+++ src/main/java/com/iu/kmi/entities/RechnungsPosition.java
@@ -0,0 +1,44 @@
+package com.iu.kmi.entities;
+import com.iu.kmi.database.annotations.*;
+
+
+@Entity(tableName = "rechnungsposition")
+public class RechnungsPosition {
+
+    @Id(name = "rechnungsposition_nr")
+    private String rechnungspositionNr;
+
+    @JoinColumn(name = "rechnung_nr", referencedColumnName = "rechnung_nr")
+    private Rechnung rechnungNr;
+
+    @JoinColumn(name = "artikel_nr", referencedColumnName = "artikel_nr")
+    private Material artikelNr;
+
+    public RechnungsPosition() {}
+
+    // Getter und Setter
+    public String getRechnungspositionNr() {
+        return rechnungspositionNr;
+    }
+
+    public void setRechnungspositionNr(String rechnungspositionNr) {
+        this.rechnungspositionNr = rechnungspositionNr;
+    }
+
+    public Rechnung getRechnungNr() {
+        return rechnungNr;
+    }
+
+    public void setRechnungNr(Rechnung rechnungNr) {
+        this.rechnungNr = rechnungNr;
+    }
+
+    public Material getArtikelNr() {
+        return artikelNr;
+    }
+
+    public void setArtikelNr(Material artikelNr) {
+        this.artikelNr = artikelNr;
+    }
+
+}
