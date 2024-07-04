d49cb1d23157847dc84ca2e55525de71b30c71dc
JH8s <julian.helaoui@web.de>
2024-07-04 20:20:50 +0200

    new commit

--- src/main/java/com/iu/kmi/entities/Kundenauftrag.java
+++ src/main/java/com/iu/kmi/entities/Kundenauftrag.java
@@ -7,26 +7,31 @@ import com.iu.kmi.database.annotations.Column;
 import com.iu.kmi.database.annotations.Entity;
 import com.iu.kmi.database.annotations.Id;
 import com.iu.kmi.database.annotations.JoinColumn;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 /**
  *
  * @author Julian
  */
-@Entity(tableName = "kundenanfrage")
+@Entity(tableName = "terminauftrag")
 public class Kundenauftrag {
-    @Id(name = "kundenanfrage_nr")
+    @Id(name = "terminauftrag_nr")
     private String kundenauftragNummer;
     @JoinColumn(name = "kunde_nr", referencedColumnName = "kunde_nr")
-    private String kundenNummer;
-    @Column(name = "anfragedatum")
-    private String anfragedatum;
-    @Column(name = "anfragebeschreibung")
-    private String anfragebeschreibung;
-    @Column(name = "prioritaet")
-    private String prioritaet;
+    private Kunde kundenNummer;
+    @Column(name = "auftragsdatum")
+    private LocalDateTime auftragsdatum;
+    @Column(name = "lieferdatum")
+    private LocalDateTime lieferdatum;
+    @JoinColumn(name = "angebot_nr", referencedColumnName = "angebot_nr")
+    private Angebot angebot_nr;
     @Column(name = "status")
     private String status;
+
+    public Kundenauftrag() throws IllegalArgumentException{
+        throw new IllegalArgumentException();
+    }
 
     @Override
     public boolean equals(Object obj) {
@@ -46,30 +51,30 @@ public class Kundenauftrag {
         if (!Objects.equals(this.kundenNummer, other.kundenNummer)) {
             return false;
         }
-        if (!Objects.equals(this.anfragedatum, other.anfragedatum)) {
+        if (!Objects.equals(this.angebot_nr, other.angebot_nr)) {
             return false;
         }
-        if (!Objects.equals(this.anfragebeschreibung, other.anfragebeschreibung)) {
+        if (!Objects.equals(this.status, other.status)) {
             return false;
         }
-        if (!Objects.equals(this.prioritaet, other.prioritaet)) {
+        if (!Objects.equals(this.auftragsdatum, other.auftragsdatum)) {
             return false;
         }
-        return Objects.equals(this.status, other.status);
-    }
-
-    public Kundenauftrag(String kundenauftragNummer, String kundenNummer, String anfragedatum, String anfragebeschreibung, String prioritaet, String status) {
-        this.kundenauftragNummer = kundenauftragNummer;
-        this.kundenNummer = kundenNummer;
-        this.anfragedatum = anfragedatum;
-        this.anfragebeschreibung = anfragebeschreibung;
-        this.prioritaet = prioritaet;
-        this.status = status;
+        return Objects.equals(this.lieferdatum, other.lieferdatum);
     }
 
     @Override
     public String toString() {
-        return "Kundenauftrag{" + "kundenauftragNummer=" + kundenauftragNummer + ", kundenNummer=" + kundenNummer + ", anfragedatum=" + anfragedatum + ", anfragebeschreibung=" + anfragebeschreibung + ", prioritaet=" + prioritaet + ", status=" + status + '}';
+        return "Kundenauftrag{" + "kundenauftragNummer=" + kundenauftragNummer + ", kundenNummer=" + kundenNummer + ", auftragsdatum=" + auftragsdatum + ", lieferdatum=" + lieferdatum + ", angebot_nr=" + angebot_nr + ", status=" + status + '}';
+    }
+
+    public Kundenauftrag(String kundenauftragNummer, Kunde kundenNummer, LocalDateTime auftragsdatum, LocalDateTime lieferdatum, Angebot angebot_nr, String status) {
+        this.kundenauftragNummer = kundenauftragNummer;
+        this.kundenNummer = kundenNummer;
+        this.auftragsdatum = auftragsdatum;
+        this.lieferdatum = lieferdatum;
+        this.angebot_nr = angebot_nr;
+        this.status = status;
     }
 
     public void setKundenauftragNummer(String kundenauftragNummer) {
@@ -80,16 +85,16 @@ public class Kundenauftrag {
         this.kundenNummer = kundenNummer;
     }
 
-    public void setAnfragedatum(String anfragedatum) {
-        this.anfragedatum = anfragedatum;
+    public void setAuftragsdatum(LocalDateTime auftragsdatum) {
+        this.auftragsdatum = auftragsdatum;
     }
 
-    public void setAnfragebeschreibung(String anfragebeschreibung) {
-        this.anfragebeschreibung = anfragebeschreibung;
+    public void setLieferdatum(LocalDateTime lieferdatum) {
+        this.lieferdatum = lieferdatum;
     }
 
-    public void setPrioritaet(String prioritaet) {
-        this.prioritaet = prioritaet;
+    public void setAngebot_nr(String angebot_nr) {
+        this.angebot_nr = angebot_nr;
     }
 
     public void setStatus(String status) {
@@ -104,21 +109,21 @@ public class Kundenauftrag {
         return kundenNummer;
     }
 
-    public String getAnfragedatum() {
-        return anfragedatum;
+    public LocalDateTime getAuftragsdatum() {
+        return auftragsdatum;
     }
 
-    public String getAnfragebeschreibung() {
-        return anfragebeschreibung;
+    public LocalDateTime getLieferdatum() {
+        return lieferdatum;
     }
 
-    public String getPrioritaet() {
-        return prioritaet;
+    public String getAngebot_nr() {
+        return angebot_nr;
     }
 
     public String getStatus() {
         return status;
     }
-    
-    
 }
+
+   
